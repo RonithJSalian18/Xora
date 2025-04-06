ebb1607d18bdef57dfa9b48ca28bf9b0ecd404bf
RonithJSalian18 <ronithjsalian01@gmail.com>
2025-04-06 20:00:40 +0530

    Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./sections/header";
+import Hero from "./sections/Hero";
 
 const App = () => {
   return (
     <main className="overflow-hidden">
       <Header />
+      <Hero />
     </main>
   );
 };
